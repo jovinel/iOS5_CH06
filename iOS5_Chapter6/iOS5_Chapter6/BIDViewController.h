adb173a42474b03def25591507dfc3b5127a9ac7
Jovi Nel <jnp.parilla@gmail.com>
2013-11-28 16:17:00 +0800

    First commit, initialize files for application
    Signed-off-by: Jovi Nel <jnp.parilla@gmail.com>

---
+++ iOS5_Chapter6/iOS5_Chapter6/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  iOS5_Chapter6
+//
+//  Created by Maria Alice C. Lim on 11/28/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end

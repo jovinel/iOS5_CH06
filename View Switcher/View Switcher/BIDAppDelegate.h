de57b93b2cc0e3a853a4a14fadef4b8b4ccfda71
Jovi Nel <jnp.parilla@gmail.com>
2013-11-28 16:45:26 +0800

    Initialize files for application

---
+++ View Switcher/View Switcher/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  View Switcher
+//
+//  Created by Maria Alice C. Lim on 11/28/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
--- iOS5_Chapter6/iOS5_Chapter6/BIDAppDelegate.h
+++ iOS5_Chapter6/iOS5_Chapter6/BIDAppDelegate.h
@@ -8,12 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@class BIDViewController;
-
 @interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (strong, nonatomic) BIDViewController *viewController;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 @end

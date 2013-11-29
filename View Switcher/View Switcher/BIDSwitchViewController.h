8e1d94a9e0bd3fa3781cfc3858e13a836061a408
Jovi Nel <jnp.parilla@gmail.com>
2013-11-29 11:02:35 +0800

    Signed-off-by: Jovi Nel <jnp.parilla@gmail.com>

---
+++ View Switcher/View Switcher/BIDSwitchViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDSwitchViewController.h
+//  View Switcher
+//
+//  Created by Maria Alice C. Lim on 11/29/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDSwitchViewController : UIViewController
+
+@end
---
+++ View Switcher/View Switcher/BIDYellowViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDYellowViewController.h
+//  View Switcher
+//
+//  Created by Maria Alice C. Lim on 11/29/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDYellowViewController : UIViewController
+
+@end

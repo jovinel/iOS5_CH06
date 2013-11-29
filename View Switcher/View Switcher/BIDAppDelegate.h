f1b7598c7d7b233075239a6c95a309fb93094e84
Jovi Nel <jnp.parilla@gmail.com>
2013-11-29 11:03:41 +0800

    Added interface and its method implementation for the 3 controllers.
    Signed-off-by: Jovi Nel <jnp.parilla@gmail.com>

--- View Switcher/View Switcher/BIDAppDelegate.h
+++ View Switcher/View Switcher/BIDAppDelegate.h
@@ -7,9 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
-
+@class BIDSwitchViewController;
 @interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) BIDSwitchViewController *switchViewController;
 
 @end
